3884cfb518e3317387cdc485316e8c2c338d023e
stuartgeller <99045056+stuartgeller@users.noreply.github.com>
2023-03-29 13:56:05 -0600

    chore: add some styling

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,18 @@
-import Link from "next/link";
-import {useState} from "react";
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Home(props) {
-  const [validUser, setValidUser] = useState(true)
-  const [accountNumber, setAccountNumber] = useState(props?.value ?? "")
-  const [accountPin, setAccountPin] = useState("")
+  const [validUser, setValidUser] = useState(true);
+  const [accountNumber, setAccountNumber] = useState(props?.value ?? '');
+  const [accountPin, setAccountPin] = useState('');
 
-  const validateUser = () => {
-
-  }
+  const validateUser = () => {};
 
   return (
     <div>
+      <h1 className="atm-title">Welcome to the CIS ATM</h1>
       {!validUser && (
         <div>
-          <h1>Welcome to the CIS ATM</h1>
           <form>
             <h2>Enter your account number and pin</h2>
             <label htmlFor="account-number">Account Number:</label>
@@ -40,18 +38,26 @@ export default function Home(props) {
         </div>
       )}
       {validUser && (
-        <div>
-          <h1>What type of transaction would you like to make?</h1>
-          <Link href="./withdraw">
-            <button>Withdraw</button>
-          </Link>
-          <Link href="./deposit">
-            <button>Deposit</button>
-          </Link>
-          <Link href="./inquire">
-            <button>Check balance</button>
-          </Link>
-        </div>
+        <>
+          <h1 className="atm-title">What type of transaction would you like to make?</h1>
+          <div className="home-links">
+            <div className="home-link__link">
+              <Link href="./withdraw">
+                <button className="atm-button">Withdraw</button>
+              </Link>
+            </div>
+            <div className="home-link__link">
+              <Link href="./deposit">
+                <button className="atm-button">Deposit</button>
+              </Link>
+            </div>
+            <div className="home-link__link">
+              <Link href="./inquire">
+                <button className="atm-button">Check balance</button>
+              </Link>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
